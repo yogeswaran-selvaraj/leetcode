607f7a89720619da51196d6efe1f86faaf3451a6
Yoges-s <76592344+Yoges-s@users.noreply.github.com>
2023-09-15 14:03:10 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (95.09%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.java
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.java
@@ -10,32 +10,11 @@
  */
 class Solution {
     public int getDecimalValue(ListNode head) {
-        head = reverList(head);
         int ans = 0;
-        ListNode cur = head;
-        int multiple = 1;
-        while(cur != null){
-            if(cur.val == 1){
-                ans += multiple;
-            }
-            
-            cur = cur.next;
-            multiple *= 2;
+        while(head!=null){
+            ans=(ans<<1)+head.val;
+            head=head.next;
         }
-        
         return ans;
-    }
-    
-    ListNode reverList( ListNode A){
-        ListNode cur = A;
-        ListNode prev = null;
-        while(cur != null){
-            ListNode temp = cur.next;
-            cur.next = prev;
-            prev = cur;
-            cur = temp;
-        }
-        
-        return prev;
     }
 }
