a188dc0e98aedcd6415ce865de1a2c665c6fa831
Yoges-s <76592344+Yoges-s@users.noreply.github.com>
2023-12-13 08:26:00 +0530

    Added solution - LeetHub

---
+++ Prime Number - GFG/prime-number.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.isPrime(N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution{
+    static int isPrime(int N){
+        // code here
+        
+        if(factors(N) == 2){
+            return 1;
+        }
+        
+        return 0;
+    }
+    
+    static int factors(int N){
+        int count = 0;
+        for(int i=1; i * i <= N; i++){
+            if(N % i == 0){
+                if(N/i == i){
+                    count++;
+                }
+                else{
+                    count += 2;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
