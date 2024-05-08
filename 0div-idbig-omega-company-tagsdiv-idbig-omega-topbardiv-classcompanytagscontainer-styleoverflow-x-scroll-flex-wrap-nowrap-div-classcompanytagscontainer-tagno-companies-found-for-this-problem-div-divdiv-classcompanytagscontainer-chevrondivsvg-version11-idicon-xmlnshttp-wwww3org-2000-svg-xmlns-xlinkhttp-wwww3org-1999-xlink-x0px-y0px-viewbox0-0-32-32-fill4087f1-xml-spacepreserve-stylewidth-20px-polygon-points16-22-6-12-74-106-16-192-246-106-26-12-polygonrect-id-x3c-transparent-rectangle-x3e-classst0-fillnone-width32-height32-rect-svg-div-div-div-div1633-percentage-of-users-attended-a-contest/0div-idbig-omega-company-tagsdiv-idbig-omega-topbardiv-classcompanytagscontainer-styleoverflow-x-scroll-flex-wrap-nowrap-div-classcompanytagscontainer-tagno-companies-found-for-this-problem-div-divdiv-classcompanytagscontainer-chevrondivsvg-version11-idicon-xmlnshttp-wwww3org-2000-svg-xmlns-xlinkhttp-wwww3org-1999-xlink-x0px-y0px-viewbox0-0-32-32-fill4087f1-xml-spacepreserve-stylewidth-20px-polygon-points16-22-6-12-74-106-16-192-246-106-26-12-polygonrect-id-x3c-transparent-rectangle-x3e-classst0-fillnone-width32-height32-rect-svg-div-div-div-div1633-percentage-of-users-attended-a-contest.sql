19a99d4c0e8dae966612fb0256d6462ffba4c134
yogeswaran <76592344+yogeswaran-selvaraj@users.noreply.github.com>
2024-05-08 09:07:13 +0530

    Time: 1921 ms (47.45%), Space: 0B (100.00%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1633-percentage-of-users-attended-a-contest/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1633-percentage-of-users-attended-a-contest.sql
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1633-percentage-of-users-attended-a-contest/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1633-percentage-of-users-attended-a-contest.sql
@@ -1,8 +1,20 @@
-# Write your MySQL query statement below
+# # Write your MySQL query statement below
 
-SELECT contest_id, ROUND((COUNT(contest_id) / (SELECT COUNT(*) FROM Users)) * 100, 2) AS percentage
-FROM Users u
-RIGHT JOIN Register r
-ON r.user_id = u.user_id
-GROUP BY contest_id
-ORDER BY percentage DESC, contest_id ASC
+# SELECT contest_id, ROUND((COUNT(contest_id) / (SELECT COUNT(*) FROM Users)) * 100, 2) AS percentage
+# FROM Users u
+# RIGHT JOIN Register r
+# ON r.user_id = u.user_id
+# GROUP BY contest_id
+# ORDER BY percentage DESC, contest_id ASC
+
+SELECT 
+    r.contest_id,
+    ROUND(COUNT(DISTINCT r.user_id) * 100 / total_users.total_count, 2) AS percentage
+FROM 
+    Register r,
+(
+    SELECT COUNT(DISTINCT user_id) AS total_count FROM Users
+) AS total_users
+GROUP BY 
+    r.contest_id
+ORDER BY percentage DESC, r.contest_id ASC
